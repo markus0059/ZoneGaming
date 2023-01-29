a86bd869943bda63b03a8dd8426fe57483a9b5a6
Germe Marc Antoine <markus0059@hotmail.fr>
2023-01-29 20:38:14 +0100

    add footer and finished hommepage

--- src/Controller/UniverseController.php
+++ src/Controller/UniverseController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/universe', name: 'universe_')]
 class UniverseController extends AbstractController
 {
-    #[Route('/universe/', name: 'universe_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
       return $this->render('universe/index.html.twig', [
         'zonegaming' => 'Zone Gaming',
      ]);
+    }
+
+    #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
+    public function show($id):Response
+    {
+      return $this->render('universe/show.html.twig', ['id' => $id]);
     }
 }
